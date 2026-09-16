87875778d73d16e2d3a1b330d0f839faa8128886
Abhishek Chakraborty <83869924+Abhishek993-10@users.noreply.github.com>
2026-09-16 12:20:41 +0530

    Add files via upload

---
+++ rps-game.js
@@ -0,0 +1,65 @@
+console.log("hi!");
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (userInput === "rock" || userInput === "paper" || userInput === "scissors" || userInput === "bomb") {
+    return userInput;
+  } else {
+    console.log("Invalid input!");
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+        break;
+    default:
+        return "Error!";
+        break;
+  }
+};
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "The game is a tie!";
+  } else if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+    } else if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
+        return "The computer won!";
+      } else {
+        return "You won!";
+      }
+    } else if (userChoice === "scissors") {
+      if (computerChoice === "rock") {
+        return "The computer won!";
+      } else {
+        return "You won!";
+      }
+    } else {
+        if (userChoice === "bomb") {
+          return "You won!";
+        }
+    }
+  };
+
+const playGame = () => {
+    const userChoice = getUserChoice("rock");
+    const computerChoice = getComputerChoice();
+    console.log(`You threw: ${userChoice}`);
+    console.log(`The computer threw: ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
